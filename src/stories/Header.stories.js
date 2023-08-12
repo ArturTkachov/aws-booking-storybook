6fd9175a0d98facba3149330ad9dd89049e0f19b
Artur Tkachov <arturtkachov22@gmail.com>
2023-08-12 18:17:18 +0300

    package test

---
+++ src/stories/Header.stories.js
@@ -0,0 +1,20 @@
+import { Header } from './Header';
+const meta = {
+    title: 'Example/Header',
+    component: Header,
+    // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
+    tags: ['autodocs'],
+    parameters: {
+        // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+        layout: 'fullscreen',
+    },
+};
+export default meta;
+export const LoggedIn = {
+    args: {
+        user: {
+            name: 'Jane Doe',
+        },
+    },
+};
+export const LoggedOut = {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as path from 'path';
+import * as packageJson from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify: 'esbuild',
+    cssMinify: 'esbuild',
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: path.resolve(__dirname, 'src/main.tsx'),
+      name: 'lib',
+      // the proper extensions will be added
+      fileName: 'lib',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      // external: ['react', 'react-dom'],
+      external: Object.keys(packageJson.peerDependencies),
+      // output: {
+      //   // Provide global variables to use in the UMD build
+      //   // for externalized deps
+      //   globals: {
+      //     vue: 'Vue',
+      //   },
+      // },
+    },
+  },
   plugins: [react()],
 })
